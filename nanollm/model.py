5e371482e9d8d4f40ace2f61ce5f178233cf4b25
amulil <amulil@126.com>
2024-05-14 23:57:03 +0800

    add naive transformer decoder block

--- nanollm/model.py
+++ nanollm/model.py
@@ -55,3 +55,32 @@ class CausalSelfAttention(nn.Module):
         
         y = self.resid_dropout(self.c_proj(y))
         return y
+    
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # why 4 * config.n_embd?
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+        
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+    
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = self.LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = self.CausalSelfAttention(config)
+        self.ln_2 = self.LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+        
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
